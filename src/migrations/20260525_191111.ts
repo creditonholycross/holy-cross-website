305c1f246e0cf05527854efd4aca592d26b8b244
kippz07 <kippz07@googlemail.com>
2026-05-25 20:58:10 +0100

    db migration

--- src/migrations/20260525_191111.ts
+++ src/migrations/20260525_191111.ts
@@ -4,27 +4,13 @@ export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
    CREATE TYPE "public"."enum_pages_blocks_text_image_block_image_size" AS ENUM('square', 'full');
   CREATE TYPE "public"."enum__pages_v_blocks_text_image_block_image_size" AS ENUM('square', 'full');
-  ALTER TABLE "pages_blocks_content_columns" ADD COLUMN "media_id" integer;
   ALTER TABLE "pages_blocks_text_image_block" ADD COLUMN "image_size" "enum_pages_blocks_text_image_block_image_size" DEFAULT 'square';
-  ALTER TABLE "_pages_v_blocks_content_columns" ADD COLUMN "media_id" integer;
-  ALTER TABLE "_pages_v_blocks_text_image_block" ADD COLUMN "image_size" "enum__pages_v_blocks_text_image_block_image_size" DEFAULT 'square';
-  ALTER TABLE "pages_blocks_content_columns" ADD CONSTRAINT "pages_blocks_content_columns_media_id_media_id_fk" FOREIGN KEY ("media_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
-  ALTER TABLE "_pages_v_blocks_content_columns" ADD CONSTRAINT "_pages_v_blocks_content_columns_media_id_media_id_fk" FOREIGN KEY ("media_id") REFERENCES "public"."media"("id") ON DELETE set null ON UPDATE no action;
-  CREATE INDEX "pages_blocks_content_columns_media_idx" ON "pages_blocks_content_columns" USING btree ("media_id");
-  CREATE INDEX "_pages_v_blocks_content_columns_media_idx" ON "_pages_v_blocks_content_columns" USING btree ("media_id");`)
+  ALTER TABLE "_pages_v_blocks_text_image_block" ADD COLUMN "image_size" "enum__pages_v_blocks_text_image_block_image_size" DEFAULT 'square';`)
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
-  await db.execute(sql`
-   ALTER TABLE "pages_blocks_content_columns" DROP CONSTRAINT "pages_blocks_content_columns_media_id_media_id_fk";
-  
-  ALTER TABLE "_pages_v_blocks_content_columns" DROP CONSTRAINT "_pages_v_blocks_content_columns_media_id_media_id_fk";
-  
-  DROP INDEX "pages_blocks_content_columns_media_idx";
-  DROP INDEX "_pages_v_blocks_content_columns_media_idx";
-  ALTER TABLE "pages_blocks_content_columns" DROP COLUMN "media_id";
+  await db.execute(sql`  
   ALTER TABLE "pages_blocks_text_image_block" DROP COLUMN "image_size";
-  ALTER TABLE "_pages_v_blocks_content_columns" DROP COLUMN "media_id";
   ALTER TABLE "_pages_v_blocks_text_image_block" DROP COLUMN "image_size";
   DROP TYPE "public"."enum_pages_blocks_text_image_block_image_size";
   DROP TYPE "public"."enum__pages_v_blocks_text_image_block_image_size";`)
