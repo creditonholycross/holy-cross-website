7966b8d12c161fa157ca2f35b27c5828a82dba8c
kippz07 <kippz07@googlemail.com>
2026-05-25 18:56:24 +0100

    prerender

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20260525_171318 from './20260525_171318';
-import * as migration_20260525_171436 from './20260525_171436';
+import * as migration_20260525_171436 from './20260525_171436'
 
 export const migrations = [
   {
-    up: migration_20260525_171318.up,
-    down: migration_20260525_171318.down,
-    name: '20260525_171318',
-  },
-  {
     up: migration_20260525_171436.up,
     down: migration_20260525_171436.down,
-    name: '20260525_171436'
+    name: '20260525_171436',
   },
-];
+]
